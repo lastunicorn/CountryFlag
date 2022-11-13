0ca08831eecca6739af92f45ed4b844de8efa319
Alexandru Iuga <alex.iuga@google.com>
2022-11-13 11:23:04 +0200

    first commit added a and b countries.

---
+++ sources/CountryFlags/Countries.A.cs
@@ -0,0 +1,170 @@
+﻿// Country Flags
+// Copyright (C) 2022 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace DustInTheWind.CountryFlags;
+
+public static partial class Countries
+{
+    public static Country Afganistan_IslamicEmirate { get; } = new()
+    {
+        ShortName = "Afghanistan",
+        FullName = "Islamic Emirate of Afghanistan"
+    };
+
+    public static Country Afganistan_IslamicRepublic { get; } = new()
+    {
+        ShortName = "Afghanistan",
+        FullName = "Islamic Republic of Afghanistan",
+        IsoCodeAlpha2 = "AF",
+        IsoCodeAlpha3 = "AFG",
+        IsoCodeNumeric = "004"
+    };
+
+    public static Country Aland { get; } = new()
+    {
+        ShortName = "Åland",
+        FullName = "Åland",
+        IsoCodeAlpha2 = "AX",
+        IsoCodeAlpha3 = "ALA",
+        IsoCodeNumeric = "248"
+    };
+
+    public static Country Albania { get; } = new()
+    {
+        ShortName = "Albania",
+        FullName = "Islamic Republic of Afghanistan",
+        IsoCodeAlpha2 = "AL",
+        IsoCodeAlpha3 = "ALB",
+        IsoCodeNumeric = "008"
+    };
+
+    public static Country Algeria { get; } = new()
+    {
+        ShortName = "Algeria",
+        FullName = "People's Democratic Republic of Algeria",
+        IsoCodeAlpha2 = "DZ",
+        IsoCodeAlpha3 = "DZA",
+        IsoCodeNumeric = "012"
+    };
+
+    public static Country AmericanSamoa { get; } = new()
+    {
+        ShortName = "American Samoa",
+        FullName = "American Samoa",
+        IsoCodeAlpha2 = "AS",
+        IsoCodeAlpha3 = "ASM",
+        IsoCodeNumeric = "016"
+    };
+
+    public static Country Andorra { get; } = new()
+    {
+        ShortName = "Andorra",
+        FullName = "Principality of Andorra",
+        IsoCodeAlpha2 = "AD",
+        IsoCodeAlpha3 = "AND",
+        IsoCodeNumeric = "020"
+    };
+
+    public static Country Angola { get; } = new()
+    {
+        ShortName = "Angola",
+        FullName = "Republic of Angola",
+        IsoCodeAlpha2 = "AO",
+        IsoCodeAlpha3 = "AGO",
+        IsoCodeNumeric = "024"
+    };
+
+    public static Country Anguilla { get; } = new()
+    {
+        ShortName = "Anguilla",
+        FullName = "Republic of Angola",
+        IsoCodeAlpha2 = "AI",
+        IsoCodeAlpha3 = "AIA",
+        IsoCodeNumeric = "660"
+    };
+
+    public static Country Antarctica { get; } = new()
+    {
+        ShortName = "Antarctica",
+        FullName = "Antarctica",
+        IsoCodeAlpha2 = "AQ",
+        IsoCodeAlpha3 = "ATA",
+        IsoCodeNumeric = "010"
+    };
+
+    public static Country AntiguaAndBarbuda { get; } = new()
+    {
+        ShortName = "Antigua and Barbuda",
+        FullName = "Antigua and Barbuda",
+        IsoCodeAlpha2 = "AG",
+        IsoCodeAlpha3 = "ATG",
+        IsoCodeNumeric = "028"
+    };
+
+    public static Country Argentina { get; } = new()
+    {
+        ShortName = "Argentina",
+        FullName = "Argentine Republic",
+        IsoCodeAlpha2 = "AR",
+        IsoCodeAlpha3 = "ARG",
+        IsoCodeNumeric = "032"
+    };
+
+    public static Country Armenia { get; } = new()
+    {
+        ShortName = "Armenia",
+        FullName = "Republic of Armenia",
+        IsoCodeAlpha2 = "AM",
+        IsoCodeAlpha3 = "ARM",
+        IsoCodeNumeric = "051"
+    };
+
+    public static Country Aruba { get; } = new()
+    {
+        ShortName = "Aruba",
+        FullName = "Country of Aruba",
+        IsoCodeAlpha2 = "AW",
+        IsoCodeAlpha3 = "ABW",
+        IsoCodeNumeric = "533"
+    };
+
+    public static Country Australia { get; } = new()
+    {
+        ShortName = "Australia",
+        FullName = "Commonwealth of Australia",
+        IsoCodeAlpha2 = "AU",
+        IsoCodeAlpha3 = "AUS",
+        IsoCodeNumeric = "036"
+    };
+
+    public static Country Austria { get; } = new()
+    {
+        ShortName = "Austria",
+        FullName = "Republic of Austria",
+        IsoCodeAlpha2 = "AT",
+        IsoCodeAlpha3 = "AUT",
+        IsoCodeNumeric = "040"
+    };
+
+    public static Country Azerbaijan { get; } = new()
+    {
+        ShortName = "Azerbaijan",
+        FullName = "Republic of Azerbaijan",
+        IsoCodeAlpha2 = "AZ",
+        IsoCodeAlpha3 = "AZE",
+        IsoCodeNumeric = "031"
+    };
+}
