ceb54f7ab7377559cd16f960cc66bb952f18848f
Alexandru Iuga <alex.iuga@gmail.com>
2024-03-22 16:38:56 +0200

    tests code cleanup

--- sources/SvgToXaml.Tests/ViewBoxTests/OriginXTests/OriginXTests.cs
+++ sources/SvgToXaml.Tests/ViewBoxTests/OriginXTests/OriginXTests.cs
@@ -14,11 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-using System.Windows.Controls;
-using DustInTheWind.SvgToXaml.Svg;
-using DustInTheWind.SvgToXaml.Tests.Utils;
-using FluentAssertions;
-
 namespace DustInTheWind.SvgToXaml.Tests.ViewBoxTests.OriginXTests;
 
 public class OriginXTests
